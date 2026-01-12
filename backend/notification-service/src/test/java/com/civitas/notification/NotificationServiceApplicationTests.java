521d0fc6e7af0c065ddf5f07a18cd2103878a3bd
Luigidrc <151552732+Luigidrc@users.noreply.github.com>
2026-01-12 12:16:04 +0100

    Setup Backend: Inseriti microservizi Spring Boot (Java 21)

---
+++ backend/notification-service/src/test/java/com/civitas/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.civitas.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
